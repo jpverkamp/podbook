ea27d674411ba2ee74ef5a8f9b60a9d43b3f8007
JP Verkamp <me@jverkamp.com>
2016-06-14 17:18:04 -0400

    Add basic authentication to directory list if required

--- server/podbook.py
+++ server/podbook.py
@@ -2,30 +2,74 @@
 
 import feedgen.feed
 import flask
+import functools
 import os
 import sys
+import uuid
 import yaml
 
-DEBUG_MODE = '--debug' in sys.argv or os.environ.get('DEBUG', False)
+DEBUG_MODE = os.environ.get('DEBUG', False)
+
+UUID_NAMESPACE = uuid.UUID(os.environ.get('UUID_NAMESPACE', uuid.uuid4()))
+
+AUTH_USERNAME = os.environ.get('AUTH_USERNAME', False)
+AUTH_PASSWORD = os.environ.get('AUTH_PASSWORD', False)
+REQUIRE_AUTH = AUTH_USERNAME and AUTH_PASSWORD
+
+if DEBUG_MODE: print('*** RUNNING IN DEBUG MODE ***')
+if not REQUIRE_AUTH: print('*** RUNNING WITHOUT AUTHENTICATION ***')
 
 app = flask.Flask(__name__)
 
+def requires_auth(f):
+    '''If we set a username and password, require it for this request.'''
+
+    @functools.wraps(f)
+    def decorated(*args, **kwargs):
+        if REQUIRE_AUTH:
+            auth = flask.request.authorization
+            if not auth or AUTH_USERNAME != auth.username or AUTH_PASSWORD != auth.password:
+                return flask.Response('Must authenticate', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+        return f(*args, **kwargs)
+    return decorated
+
+def book_to_uuid(book):
+    '''Translate a book folder into a deterministic UUID.'''
+
+    return uuid.uuid5(UUID_NAMESPACE, book)
+
+def uuid_to_book(id, cache = {}):
+    '''Translate a UUID from above back into a book folder.'''
+
+    if not isinstance(id, uuid.UUID):
+        id = uuid.UUID(id)
+
+    if not id in cache:
+        cache.clear()
+        for book in os.listdir('books'):
+            cache[book_to_uuid(book)] = book
+
+    if not id in cache:
+        raise Exception('{} does not match any known book'.format(id))
+
+    return cache[id]
+
 @app.route('/')
+@requires_auth
 def index():
     result = '<ul>'
 
-    for path in sorted(os.listdir('books')):
-        config_path = os.path.join('books', path, 'book.yml')
-        print(config_path)
-
+    for book in sorted(os.listdir('books')):
+        config_path = os.path.join('books', book, 'book.yml')
         if not os.path.exists(config_path):
             continue
 
         with open(config_path, 'r') as fin:
             config = yaml.load(fin)
 
-        result += '<li><a href="feed/{path}.xml">{title} by {author}</a></li>'.format(
-            path = path,
+        result += '<li><a href="feed/{uuid}.xml">{title} by {author}</a></li>'.format(
+            uuid = book_to_uuid(book),
             title = config['title'],
             author = config['author']
         )
@@ -33,9 +77,10 @@ def index():
     result += '</ul>'
     return result
 
-@app.route('/feed/<feed>.xml')
-def get_feed(feed):
-    config_path = os.path.join('books', feed, 'book.yml')
+@app.route('/feed/<uuid>.xml')
+def get_feed(uuid):
+    book = uuid_to_book(uuid)
+    config_path = os.path.join('books', book, 'book.yml')
 
     with open(config_path, 'r') as fin:
         config = yaml.load(fin)
@@ -45,7 +90,7 @@ def get_feed(feed):
 
     host_url = flask.request.scheme + '://' + flask.request.host
 
-    feed_link = host_url + '/feed/{feed}.xml'.format(feed = feed)
+    feed_link = host_url + '/feed/{uuid}.xml'.format(uuid = uuid)
 
     fg.id = feed_link
     fg.title(config['title'])
@@ -55,13 +100,13 @@ def get_feed(feed):
 
     fg.podcast.itunes_category('Arts')
 
-    for file in sorted(os.listdir(os.path.join('books', feed))):
+    for file in sorted(os.listdir(os.path.join('books', book))):
         if not file.endswith('.mp3'):
             continue
 
         name = file.rsplit('.', 1)[0]
 
-        feed_entry_link = host_url + '/feed/{feed}/{file}'.format(feed = feed, file = file)
+        feed_entry_link = host_url + '/media/{book}/{file}'.format(book = book, file = file)
         feed_entry_link = feed_entry_link.replace(' ', '%20')
 
         fe = fg.add_entry()
